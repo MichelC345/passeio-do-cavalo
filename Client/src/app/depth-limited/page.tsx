39184e5d027b7262cb19f41d42a9be4e9c0a88fd
MichelC345 <michel.caesarr@gmail.com>
2024-12-05 01:37:22 -0300

    Agora é possível determinar o tamanho do tabuleiro e a posição inicial no algoritmo de busca em profundidade.

--- Client/src/app/depth-limited/page.tsx
+++ Client/src/app/depth-limited/page.tsx
@@ -11,17 +11,21 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-const socket = io("http://localhost:5000"); // Backend WebSocket URL
-const n = 5;
+const socket = io("http://localhost:5000"); // Conexão estabelecida via WebSocket
+const n = 5; // Valor de N, por padrão 5
 
 export default function Home() {
+  const [inputN, setInputN] = useState(5);
+  const [n, setN] = useState(5);
   const [board, setBoard] = useState(Array(n).fill(Array(n).fill(-1)));
-  const [movements, setMovements] = useState(0);
   const [executionTime, setExecutionTime] = useState(0);
-  const [memoryUsed, setMemoryUsed] = useState("");
+  const [memoryUsed, setMemoryUsed] = useState(0);
   const [generatedNodes, setGeneratedNodes] = useState(0);
   const [visitedNodes, setVisitedNodes] = useState(0);
-  const [isFinished, setIsFinished] = useState(false); // Tracks algorithm status
+  const [isFinished, setIsFinished] = useState(false);
+  const [isPossible, setIsPossible] = useState(false);
+  const [lin, setLin] = useState(0);
+  const [col, setCol] = useState(0);
 
   useEffect(() => {
     socket.on("update_board", (data) => {
@@ -35,14 +39,17 @@ export default function Home() {
 
     socket.on("update_time", (data) => {
       setExecutionTime(data.execution_time);
-      setIsFinished(true); // Mark as finished
     });
 
     socket.on("update_memory", (data) => {
       setMemoryUsed(data.memory_used);
     });
 
-    // Cleanup on unmount
+    socket.on("execution_finished", (data) => {
+      setIsPossible(data.possible);
+      setIsFinished(true);
+    })
+
     return () => {
       socket.off("update_board");
       socket.off("update_time");
@@ -51,11 +58,40 @@ export default function Home() {
     };
   }, []);
 
+  const handleGridSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = parseInt(event.target.value, 10);
+    if (!isNaN(newSize) && newSize > 0) {
+      setInputN(newSize);
+    }
+  };
+  
+  const handleLineChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = parseInt(event.target.value, 10);
+    if (!isNaN(newSize) && newSize > 0) {
+      setLin(newSize);
+    }
+  };
+
+  const handleColumnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = parseInt(event.target.value, 10);
+    if (!isNaN(newSize) && newSize > 0) {
+      setCol(newSize);
+    }
+  };
+
+  const applyGridSize = () => {
+    setN(inputN);
+    setLin(lin);
+    setCol(col);
+    setBoard(Array(inputN).fill(Array(inputN).fill(-1)));
+    setIsFinished(false);
+  };
+
   const initHC = async () => {
     setIsFinished(false);
     setGeneratedNodes(0);
     setVisitedNodes(0);
-    socket.emit("start");
+    socket.emit("start", {n, lin, col});
   }
 
   return (
@@ -64,37 +100,92 @@ export default function Home() {
           <CardTitle>Passeio do Cavalo usando busca em profundidade limitada</CardTitle>
         </CardHeader>
         <CardContent className="justify-between items-center">
-            <div style={{
+        <div style={{ marginBottom: "20px" }}>
+          <label htmlFor="grid-size">Tamanho do tabuleiro (N): </label>
+          <input
+            id="grid-size"
+            type="number"
+            value={inputN}
+            onChange={handleGridSizeChange}
+            style={{
+              width: "50px",
+              textAlign: "center",
+              marginLeft: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              padding: "5px",
+            }}
+          />
+          <label htmlFor="grid-size" className="ml-10">Posição Inicial: </label>
+          <input
+            id="grid-size"
+            type="number"
+            value={lin}
+            onChange={handleLineChange}
+            style={{
+              width: "50px",
+              textAlign: "center",
+              marginLeft: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              padding: "5px",
+            }}
+          />
+          <input
+            id="grid-size"
+            type="number"
+            value={col}
+            onChange={handleColumnChange}
+            style={{
+              width: "50px",
+              textAlign: "center",
+              marginLeft: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              padding: "5px",
+            }}
+          />
+        </div>
+        <div style={{ marginBottom: "20px", alignItems: "center", justifyContent: "center"}}>
+          
+          <Button onClick={applyGridSize} style={{marginLeft: "40%",}}>Confirmar</Button>
+        </div>
+        
+        <div
+          style={{
             display: "grid",
-            gridTemplateColumns: `repeat(5, 50px)`,
+            gridTemplateColumns: `repeat(${n}, 50px)`,
             gap: "2px",
-          }}>
-            {board.flat().map((value, idx) => (
-              <div
-                key={idx}
-                style={{
-                  width: "50px",
-                  height: "50px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: value === -1 ? "#ddd" : "#4caf50",
-                  color: "#fff",
-                  fontWeight: "bold",
-                  border: "1px solid #ccc",
-                }}
-              >
-                {value !== -1 ? value : ""}
-              </div>
-            ))}
-          </div>
-          <Button onClick={initHC} className="mt-10">Iniciar algoritmo</Button>
+            marginLeft: "25%",
+          }}
+        >
+          {board.flat().map((value, idx) => (
+            <div
+              key={idx}
+              style={{
+                width: "50px",
+                height: "50px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: value === -1 ? "#ddd" : "#4caf50",
+                color: "#fff",
+                fontWeight: "bold",
+                border: "1px solid #ccc",
+              }}
+            >
+              {value !== -1 ? value : ""}
+            </div>
+          ))}
+        </div>
+          <Button onClick={initHC} className="mt-10" style={{marginLeft: "35%",}}>Iniciar algoritmo</Button>
         </CardContent>
         <CardFooter>
           {isFinished ? (
             <>
+              {!isPossible ? `Não foi encontrada uma solução para o tamanho ${n}`: "Solução obtida com sucesso"} <br />
               Tempo de execução: {executionTime} <br />
-              Memória utilizada: {memoryUsed} <br />
+              Memória utilizada: {memoryUsed} MB <br />
               Número de nós gerados: {generatedNodes} <br />
               Número de nós visitados: {visitedNodes} <br />
             </>
--- Server/app.py
+++ Server/app.py
@@ -6,111 +6,118 @@ import psutil
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-N = 5  # Board dimensions
-generated_nodes = 0 # Quantidade de movimentos necessários
-visited_nodes = 0
+# Variáveis globais
+N = 5 # Dimensões da borda, por padrão 5
+generated_nodes = 0 # Quantidade de nós gerados
+visited_nodes = 0 # Nós visitados
+lin = 0
+col = 0
 
+# Array que determina os possíveis movimentos que o cavalo pode fazer
 KNIGHT_MOVES = [
     (2, 1), (1, 2), (-1, 2), (-2, 1),
     (-2, -1), (-1, -2), (1, -2), (2, -1)
 ]
 
+# Função para verificar se o movimento está dentro dos limites da borda e se ainda não foi verificado
 def is_valid_move(x, y, board):
     return 0 <= x < N and 0 <= y < N and board[x][y] == -1
 
+# Função para executar a busca
 def depth_limited_search(board, x, y, move_count, depth_limit):
     global generated_nodes, visited_nodes
 
+    # Caso o movimento esteja dentro do limite estabelecido N*N, significa que todo o tabuleiro foi percorrido
+    # Portanto solução válida
     if move_count == depth_limit:
-        #updates.append([row[:] for row in board])
         return True
     
+    # Para cada chamada de método, um nó visitado
     visited_nodes += 1
 
+    # Verifica os possíveis movimentos dada a posição x,y atual
     for dx, dy in KNIGHT_MOVES:
         next_x, next_y = x + dx, y + dy
 
+        # Caso o movimento seja válido
         if is_valid_move(next_x, next_y, board):
-            generated_nodes += 1 #Para cada movimento válido, incrementa 1
-            board[next_x][next_y] = move_count
-            #updates.append([row[:] for row in board])  # Record current state
+            generated_nodes += 1 # Para cada movimento válido, incrementa 1
+            board[next_x][next_y] = move_count # Atualiza respectiva posição da borda
 
+            # Executa algoritmo de busca e retorna verdadeiro caso tenha sido concluída com sucesso
             if depth_limited_search(board, next_x, next_y, move_count + 1, depth_limit):
                 return True
 
+            # Redefine a mesma posição como não visitada para não ser incoerente nas demais chamadas
             board[next_x][next_y] = -1  # Backtrack
-            #updates.append([row[:] for row in board])  # Record backtrack state
 
+    # Caso nenhuma busca tenha sido concluída com sucesso, não foram encontradas soluções
     return False
 
+# Função para exibir a solução passo a passo no front-end
 def print_board(board):
     global generated_nodes
-    """
-    Print the chessboard in a readable format.
-    """
-    #for row in board:
-       # print(" ".join(f"{cell:2}" for cell in row))
+    
+    # Array de atualizações para armazenar todos os estados da solução
     updates = []
+    # Matriz de movimentos vai armazenar os movimentos 0, 1, 2, ..., N*N, conforme a solução obtida
     movement = [None] * N*N
     for i in range(N):
         for j in range (N):
-            #print(board[i][j])
             movement[board[i][j]] = (i, j)
     board = [[-1 for _ in range(N)] for _ in range(N)]
     mov_count = 0
+    # Para cada movimento, atualiza a borda na posição respectiva e armazena isso no array de atualizações
     for i, j in movement:
-        #print(i, j)
         board[i][j] = mov_count
         updates.append([row[:] for row in board])
         mov_count += 1
 
+    # Atualiza o front-end exibindo passo a passo os estados da solução
     for update in updates:
         socketio.emit('update_board', {'board': update})
-        time.sleep(0.5)  # Delay for frontend visualization
-    # Atualiza número de movimentos
-    #socketio.emit('update_movements', {'movements': generated_nodes})
+        time.sleep(0.5)
+
+# A rota /start determina o início do algoritmo
+@socketio.on("start")
+def start_knights_tour(data):
+    global N, lin, col
+    N = data.get("n", 5) # Recebe o tamanho de N. Por padrão será 5.
+    lin = data.get("lin", 0)
+    col = data.get("col", 0)
+    socketio.start_background_task(run_knights_tour)
+
+def run_knights_tour():
+    global N, generated_nodes, visited_nodes, lin, col
+    # Inicialização dos valores
+    possible = False
+    generated_nodes = 0
+    visited_nodes = 0
+    board = [[-1 for _ in range(N)] for _ in range(N)]
+    board[lin][col] = 0
+    
+    # Utiliza-se a biblioteca psutil para obter as informações do processo e computar a memória utilizada
+    process = psutil.Process()
+    # A biblioteca time é utilizada para obter o tempo de execução
+    start_time = time.time()
+
+    # Caso a solução tenha sido encontrada, informa os estados no front-end e atualiza a variável booleana possible
+    if depth_limited_search(board, lin, col, 1, N * N):
+        print_board(board)
+        possible = True
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time # Tempo de execução
+    memory_used = process.memory_info().rss / (1024 * 1024)  # Armazena a memória em MB
+
+    # Passa os valores obtidos para o front-end
+    socketio.emit("update_time", {"execution_time": elapsed_time})
+    socketio.emit("update_memory", {"memory_used": memory_used})
     socketio.emit('update_nodes', {
         'generated_nodes': generated_nodes,
         'visited_nodes': visited_nodes
     })
-
-'''def solve_knights_tour():
-    board = [[-1 for _ in range(N)] for _ in range(N)]
-    board[0][0] = 0
-    updates = []
-    
-    if depth_limited_search(board, 0, 0, 1, N * N, updates):
-        print_board(board)
-        return updates  # All board states
-    else:
-        return []'''
-
-#@app.route('/start')
-@socketio.on("start")
-def start_knights_tour():
-    socketio.start_background_task(run_knights_tour)
-    return "Knight's Tour started"
-
-def run_knights_tour():
-    global generated_nodes
-    generated_nodes = 0 # Inicializa quantidade de movimentos
-    visited_nodes = 0
-    board = [[-1 for _ in range(N)] for _ in range(N)]
-    board[0][0] = 0
-    
-    process = psutil.Process()
-    start_time = time.time()
-
-    if depth_limited_search(board, 0, 0, 1, N * N):
-        print_board(board)
-        #return updates  # All board states
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    memory_used = process.memory_info().rss / (1024 * 1024)  # Memory in MB
-
-    socketio.emit("update_time", {"execution_time": f"{elapsed_time:.2f} seconds"})
-    socketio.emit("update_memory", {"memory_used": f"{memory_used:.2f} MB"})
+    socketio.emit('execution_finished', {'possible': possible})
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
